c153ff01454a435b32425069308ee3f8443e291c
mzbudi <ronibudianto361@gmail.com>
2024-10-09 18:55:38 +0700

    iap implementation, ready for testing

--- lib/Donation/donation.dart
+++ lib/Donation/donation.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../Services/ads_service.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:in_app_purchase_storekit/in_app_purchase_storekit.dart'; // Untuk iOS
 
 class Donation extends StatefulWidget {
   const Donation({super.key});
@@ -10,46 +11,98 @@ class Donation extends StatefulWidget {
 }
 
 class _DonationState extends State<Donation> {
-  // final InAppPurchase _iap = InAppPurchase.instance;
-  // bool _available = true;
-  // List<ProductDetails> _products = [];
-  // final Set<String> _productIds = {
-  //   'traktir_kopi',
-  //   'traktir_nasi',
-  //   'traktir_burger',
-  //   'traktir_eskrim'
-  // };
+  final InAppPurchase _iap = InAppPurchase.instance;
+  bool _available = true;
+  List<ProductDetails> _products = [];
+  // product id from Google Play
+  final Set<String> _productIds = {
+    'jatitek_coffee',
+    'jatitek_rice',
+    'jatitek_burger',
+    'jatitek_eskrim'
+  };
 
   @override
   void initState() {
     super.initState();
-    // _initializeIAP();
+    _initializeIAP();
+    // Mendengarkan update dari pembelian
+    final purchaseUpdated = _iap.purchaseStream;
+    purchaseUpdated.listen(_onPurchaseUpdated, onError: _onPurchaseError);
   }
 
   // Inisialisasi IAP dan ambil produk dari Google Play
-  // void _initializeIAP() async {
-  //   _available = await _iap.isAvailable();
-  //   if (_available) {
-  //     ProductDetailsResponse response =
-  //         await _iap.queryProductDetails(_productIds);
-  //     if (response.notFoundIDs.isEmpty) {
-  //       setState(() {
-  //         _products = response.productDetails;
-  //       });
-  //     }
-  //   }
-  // }
+  void _initializeIAP() async {
+    _available = await _iap.isAvailable();
+    if (_available) {
+      ProductDetailsResponse response =
+          await _iap.queryProductDetails(_productIds);
+      if (response.notFoundIDs.isEmpty) {
+        setState(() {
+          _products = response.productDetails;
+
+          // Sort products based on price
+          _products.sort((a, b) {
+            double priceA = _extractNumericPrice(a.price);
+            double priceB = _extractNumericPrice(b.price);
+            return priceA.compareTo(priceB); // Sorting in ascending order
+          });
+        });
+      }
+    }
+  }
+
+  // Helper function to extract numeric value from price string
+  double _extractNumericPrice(String priceString) {
+    // Remove any non-numeric characters (e.g., '$' or 'Rp')
+    String numericString = priceString.replaceAll(RegExp(r'[^\d.]'), '');
+    return double.tryParse(numericString) ?? 0.0; // Convert to double
+  }
 
   // Fungsi untuk memulai pembelian
-  // void _buyProduct(ProductDetails product) {
-  //   final PurchaseParam purchaseParam = PurchaseParam(productDetails: product);
-  //   _iap.buyNonConsumable(
-  //       purchaseParam:
-  //           purchaseParam); // Menggunakan non-konsumabel untuk donasi satu kali
-  // }
+  void _buyProduct(ProductDetails product) {
+    final PurchaseParam purchaseParam = PurchaseParam(productDetails: product);
+    _iap.buyNonConsumable(purchaseParam: purchaseParam);
+  }
+
+  // Fungsi untuk menangani update pembelian
+  void _onPurchaseUpdated(List<PurchaseDetails> purchaseDetailsList) {
+    for (var purchase in purchaseDetailsList) {
+      if (purchase.status == PurchaseStatus.pending) {
+        // Pembelian sedang diproses
+      } else if (purchase.status == PurchaseStatus.purchased) {
+        // Pembelian berhasil
+        _iap.completePurchase(
+            purchase); // Pastikan untuk menyelesaikan pembelian
+      } else if (purchase.status == PurchaseStatus.error) {
+        // Tangani error pembelian
+      }
+    }
+  }
+
+  // Fungsi untuk menangani error pembelian
+  void _onPurchaseError(Object error) {
+    print("Error pembelian: $error");
+  }
 
   void _rewardedAdBtn() {
     AdService().showRewardedAd();
+  }
+
+  // Fungsi untuk mengembalikan teks berdasarkan product.id
+  String _getDonationText(String productId) {
+    switch (productId) {
+      case 'jatitek_coffee':
+        return 'Traktir Kopi ☕';
+      case 'jatitek_rice':
+        return 'Traktir Makan 🍚';
+      case 'jatitek_burger':
+        return 'Traktir Burger 🍔';
+      case 'jatitek_eskrim':
+        return 'Traktir Es Krim 🍨';
+      default:
+        return 'Dukungan';
+    }
   }
 
   @override
@@ -67,9 +120,7 @@ class _DonationState extends State<Donation> {
                 fontWeight: FontWeight.bold),
             textAlign: TextAlign.center,
           ),
-          const SizedBox(
-            height: 60,
-          ),
+          const SizedBox(height: 60),
           Text(
             "Traktir Developer 😍",
             style: TextStyle(
@@ -78,25 +129,9 @@ class _DonationState extends State<Donation> {
                 fontWeight: FontWeight.bold,
                 color: Theme.of(context).colorScheme.primary),
           ),
-          const SizedBox(
-            height: 8,
-          ),
-          Container(
-              margin: const EdgeInsets.only(bottom: 28),
-              height: 200,
-              child: GridView.count(
-                crossAxisCount: 2,
-                childAspectRatio: 2,
-                children: [
-                  _buildDonationCard('Traktir', '☕'),
-                  _buildDonationCard('Traktir', '🍚'),
-                  _buildDonationCard('Traktir', '🍔'),
-                  _buildDonationCard('Traktir', '🍨'),
-                ],
-              )),
-          const SizedBox(
-            height: 24,
-          ),
+          const SizedBox(height: 8),
+          _buildDonationOptions(),
+          const SizedBox(height: 24),
           const Text(
             "Bisa juga support kami dengan nonton iklan dibawah! 👍",
             style: TextStyle(
@@ -120,7 +155,23 @@ class _DonationState extends State<Donation> {
     );
   }
 
-  Widget _buildDonationCard(String label, String icon) {
+  Widget _buildDonationOptions() {
+    return _products.isNotEmpty
+        ? GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, childAspectRatio: 2),
+            itemCount: _products.length,
+            itemBuilder: (context, index) {
+              var product = _products[index];
+              return _buildDonationCard(product.title, product.price, product);
+            },
+          )
+        : const Text("Loading donation options...");
+  }
+
+  Widget _buildDonationCard(
+      String label, String price, ProductDetails product) {
     return Container(
       padding: const EdgeInsets.all(8),
       child: ElevatedButton(
@@ -128,21 +179,16 @@ class _DonationState extends State<Donation> {
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12)),
               elevation: 4),
-          onPressed: () {},
+          onPressed: () {
+            _buyProduct(product);
+          },
           child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
             Text(
-              label,
+              _getDonationText(product.id),
               style: const TextStyle(
                   fontFamily: 'Poppins',
                   fontWeight: FontWeight.bold,
-                  fontSize: 16),
-            ),
-            Text(
-              icon,
-              style: const TextStyle(
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24),
+                  fontSize: 12),
             ),
           ])),
     );
